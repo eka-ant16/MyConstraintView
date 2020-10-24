bcb9b739563812bf15b5de19dbca909c2f58b81e
eka-ant16 <muktieka44@gmail.com>
2020-10-24 11:22:16 +0700

    first commit

---
+++ app/src/main/java/com/eka/myconstraintview/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.eka.myconstraintview
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
